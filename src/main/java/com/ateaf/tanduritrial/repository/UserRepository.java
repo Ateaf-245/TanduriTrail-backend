259b37b7dbaffc75089ee4f23f4e73e3ec2aa5c3
Ateaf-245 <ateafbankapur11@gmail.com>
2025-01-12 16:49:25 +0530

    files modified or deleted

--- src/main/java/com/ateaf/tanduritrial/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ateaf.tanduritrial.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ateaf.tanduritrial.modal.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
